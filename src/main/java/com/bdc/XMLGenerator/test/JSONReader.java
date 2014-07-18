48b790d60825033e6d44fe3d037758f8266b360f
anukool <anukool@gmail.com>
2014-07-18 16:24:10 -0400

    loop

--- src/main/java/com/bdc/XMLGenerator/test/JSONReader.java
+++ src/main/java/com/bdc/XMLGenerator/test/JSONReader.java
@@ -1,6 +1,8 @@
 package com.bdc.XMLGenerator.test;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
@@ -19,19 +21,7 @@ public class JSONReader {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JSONReader test = new JSONReader();
-		test.generateXMlStartBlock(test.generateJSON());
-	}
-
-	public String generateJSON() {
-		// Service svc = new Service();
-		JSONObject json = new JSONObject();
-		json.put("id", "pipe1");
-		json.put("name", "pipeline1");
-		json.put("className", "com.test");
-		json.put("startComponent", "reader");
-
-		return json.toString();
-
+		test.generateXMlStartBlock(test.generateJSONArray());
 	}
 
 	public String generateJSONArray() {
@@ -48,16 +38,18 @@ public class JSONReader {
 		
 		json = new JSONObject();
 		json.put("id", "reader");
-		json.put("class", "com.bdc.bdi.storm.integrator.test.WordReader"); //comp header
+		json.put("className", "com.bdc.bdi.storm.integrator.test.WordReader"); //comp header
 		
 		JSONArray propArray = new JSONArray();
 		JSONObject propJson = new JSONObject();
 		
-		propJson.put("fileNamePattern", "^building.*$")	;
+		propJson.put("name","fileNamePattern")	;
+		propJson.put("value", "^building.*$")	;
 		propArray.put(propJson) ;
 		
 		propJson = new JSONObject();
-		propJson.put("fileDir", "/data/stormIntegrator/")	;
+		propJson.put("name", "fileDir")	;
+		propJson.put("value", "/data/stormIntegrator/")	;
 		propArray.put(propJson) ;
 		
 		json.put("properties", propArray); //prop list
@@ -72,7 +64,7 @@ public class JSONReader {
 		compJson.put("value", "Agg")	;
 		propArray.put(compJson) ; 
 		
-		json.put("properties", propArray); //comp list
+		json.put("components", propArray); //comp list
 		
 		array.put(json); //body
 		
@@ -86,44 +78,70 @@ public class JSONReader {
 		VelocityContext context = new VelocityContext();
 
 		
-		JSONArray array = new JSONArray(jsonString);
+	//	JSONObject incomingJSON = new JSONObject(jsonString);
+		
+		JSONArray array = new JSONArray(jsonString); //incomingJSON.getJSONArray(null);
 		
 		JSONObject header = (JSONObject) array.get(0);		
 		context.put("Pipeline", header);
+		array.remove(0);
+		
+		List<JSONWrapper> beans = new ArrayList(); 
+		
+		for (int i=0; i<array.length();i++)
+		{
+			
+			JSONWrapper wrapper = new JSONWrapper(); 
+			
+			JSONObject obj = array.getJSONObject(i); 
+			wrapper.setObject(obj);
+			
+			beans.add(wrapper); 
+			
+			List<JSONObject> properties = new ArrayList(); 
+			List<JSONObject> components = new ArrayList(); 
+			
+			JSONArray a1 = obj.getJSONArray("properties"); 
+			JSONArray a2 = obj.getJSONArray("components"); 
+			
+			for (int j=0;j<a1.length();j++)
+			{
+				properties.add((JSONObject)a1.get(j)); 
+			}
+
+			for (int j=0;j<a2.length();j++)
+			{
+				components.add((JSONObject)a2.get(j)); 
+			}
+
+			
+			wrapper.setComponents(components);
+			wrapper.setProperties(properties);
+		
+		}
+			
+			
+		
+		
+		
+		context.put("Beans", beans);
 		
 		Template template = null;
 		try {
 			template = Velocity.getTemplate("./src/main/resources/FirstComp.vm");
 		} catch (ResourceNotFoundException | ParseErrorException | MethodInvocationException rnfe) {
 			// couldn't find the template
+			rnfe.printStackTrace();
 		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		StringWriter sw = new StringWriter();
 		template.merge(context, sw);
 		System.out.println(sw.toString());
-		
-		for (int i = 1; i <= array.length(); i++) {
-			JSONObject bean = (JSONObject) array.get(0);		
-			context.put("Bean", bean);
-			
-			try {
-				template = Velocity.getTemplate("./src/main/resources/BeanComp.vm");
-			} catch (ResourceNotFoundException | ParseErrorException | MethodInvocationException rnfe) {
-				// couldn't find the template
-			} catch (Exception e) {
-			}
-
-			sw = new StringWriter();
-			template.merge(context, sw);
-		}
-		
+				
 		return null; //sw.toString();
 	}
 
 }
 
-// String xmlFile = "test.xml" ;
-// JSONArray jsonList = new JSONArray(args[0]) ;
-//
-// while (JSONObject json : JSON)
---
+++ src/main/java/com/bdc/XMLGenerator/test/JSONWrapper.java
@@ -0,0 +1,42 @@
+package com.bdc.XMLGenerator.test;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class JSONWrapper {
+	
+	JSONObject object; 
+	
+	List<JSONObject> components; 
+	
+	List <JSONObject> properties;
+
+	public JSONObject getObject() {
+		return object;
+	}
+
+	public void setObject(JSONObject object) {
+		this.object = object;
+	}
+
+	public List<JSONObject> getComponents() {
+		return components;
+	}
+
+	public void setComponents(List<JSONObject> components) {
+		this.components = components;
+	}
+
+	public List<JSONObject> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(List<JSONObject> properties) {
+		this.properties = properties;
+	} 
+	
+	
+	
+
+}
